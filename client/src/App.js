c74f6a8da949f28f23547f64f547c4db7efa9202
Jess Daniel <jessdaniel474@gmail.com>
2019-10-14 19:13:53 -0400

    Added router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import {Route} from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState( [null] );
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -12,7 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList}/>
+      <Route path="/movies/:id" component={Movie}/>
     </div>
   );
 };
